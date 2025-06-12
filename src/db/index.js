bcb5b4f502083ec3651bfedb89caa9b235863187
Manish Sanghani <mjsanghani2005@gmail.com>
2025-06-12 17:37:40 +0530

    connect database

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { YOUTUBE } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${YOUTUBE}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} \n`);
+        
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;   
